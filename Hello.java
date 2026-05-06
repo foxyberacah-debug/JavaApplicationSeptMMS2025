f512820fa7060d263d36835fb9dcb3fb3d0683a0
foxyberacah-debug <foxyberacah@gmail.com>
2026-05-06 16:26:49 +0100

    Hello

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("Hello everyone, I am learning Java and it's tiring");
+		System.out.print("Oh wow, Learning Java is fun. ");
+		System.out.print("Hurray, I'm Happy. ");
+		System.out.printf("Java was created in %d",1992);
+		System.out.printf(", it was made free for %c%.2f",'$',0.0000);
+		System.out.printf(". Do you love Java%c %b",'?',true);
+		System.out.printf("Yes learning %s is fun","Java");
+	}
+}	
